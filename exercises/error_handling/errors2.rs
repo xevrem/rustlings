2c85e549a7edcc5b0abb21b6b15d10f2303827af
erika jonell <erika.jonell@tenet3.com>
2020-02-12 15:26:42 -0500

    end of work day progress

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -23,9 +23,13 @@ use std::num::ParseIntError;
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty = match item_quantity.parse::<i32>() {
+        Ok(num) => Ok(num),
+        Err(error) => Err(error),
+    };
 
-    Ok(qty * cost_per_item + processing_fee)
+    qty
+    // Ok(qty * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]
--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,7 +5,11 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($word:expr) => {
+        "Hello ".to_string() + $word
+    };
+}
 
 fn main() {
     if my_macro!("world!") != "Hello world!" {
