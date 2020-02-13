666ba91c8c6db0184adc8d1c6de22744139eed0b
erika jonell <erika.jonell@gmail.com>
2020-02-13 11:23:29 +0000

    morning updates

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -11,7 +11,8 @@ use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers);
+    let child_numbers = Arc::clone(&shared_numbers);
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
